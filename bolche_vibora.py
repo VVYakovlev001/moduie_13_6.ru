899afec22e7938975b3c1a925911ae7f788c4186
VVYakovlev001 <slavik71_@mail.ru>
2025-01-18 20:13:00 +0200

    Add files via upload

---
+++ bolche_vibora.py
@@ -0,0 +1,120 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+
+api = "7345376595:AAHyZWBH1R5sP5J_lA4vACj1aJTG6dny2wI"
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+kb = ReplyKeyboardMarkup(resize_keyboard=True)
+button_1 = KeyboardButton(text='Рассчитать')
+button_2 = KeyboardButton(text='Информация')
+kb.add(button_1)
+kb.add(button_2)
+
+i_kb = InlineKeyboardMarkup()
+i_button_1 = InlineKeyboardButton(text='Рассчитать норму калорий', callback_data='calories')
+i_button_2 = InlineKeyboardButton(text='Формулы расчёта', callback_data='formulas')
+i_kb.add(i_button_1)
+i_kb.add(i_button_2)
+
+
+
+class UserState(StatesGroup):
+    age = State()  # возраст
+    growth = State()  # рост
+    weight = State()  # вес
+
+
+@dp.message_handler(text='рассчитать')
+async def menu(message):
+    await message.answer('Выберите опцию:', reply_markup=i_kb)
+
+
+@dp.callback_query_handler(text="formulas")
+async def get_formulas(call):
+    await call.message.answer('для мужчин: 10 х вес (кг) + 6,25 x рост (см) – 5 х возраст (г) + 5;'
+                              'для женщин: 10 x вес (кг) + 6,25 x рост (см) – 5 x возраст (г) – 161.')
+    await call.answer()
+
+
+@dp.message_handler(commands=['start'])
+async def info(message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью.',reply_markup=i_kb)
+
+
+@dp.callback_query_handler(text='calories')
+async def set_age(call):
+    await call.message.answer("Введите свой возраст:")
+    await UserState.age.set()
+    await call.answer()
+
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message, state):
+    await state.update_data(age=message.text)
+    await message.answer("Введите свой рост:")
+    await UserState.growth.set()
+
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message, state):
+    await state.update_data(growth=message.text)
+    await message.answer("Введите свой вес:")
+    await UserState.weight.set()
+
+
+@dp.message_handler(state=UserState.weight)
+async def set_calories(message, state):
+    await state.update_data(weight=message.text)
+    data = await state.get_data()
+    norm_calories_men = int(10 * int(data['weight']) + 6.25 * int(data['growth']) - 5 * int(data['age']) + 5)
+    norm_calories_vomen = int(10 * int(data['weight']) + 6.25 * int(data['growth']) - 5 * int(data['age']) - 161)
+    await message.answer(f"Мужская норма в сутки {norm_calories_men} ккал."
+                         f" Женская норма в сутки {norm_calories_vomen} ккал.")
+    await state.finish()
+
+
+@dp.message_handler()
+async def all_massages(message):
+    await message.answer('Введите команду /start, чтобы начать общение.')
+# Упрощенный вариант формулы Миффлина - Сан Жеора:
+# для мужчин: 10 х вес (кг) + 6,25 x рост (см) – 5 х возраст (г) + 5;
+# для женщин: 10 х вес (кг) + 6,25 х рост (см) - 5 х возраст (г) -161
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
+
+
+'''
+Необходимо дополнить код предыдущей задачи, чтобы при нажатии на кнопку 'Рассчитать'
+ присылалась Inline-клавиатруа.
+Создайте клавиатуру InlineKeyboardMarkup с 2 кнопками InlineKeyboardButton:
+С текстом 'Рассчитать норму калорий' и callback_data='calories'
+С текстом 'Формулы расчёта' и callback_data='formulas'
+Создайте новую функцию main_menu(message), которая:
+Будет обёрнута в декоратор message_handler, срабатывающий при передаче текста
+ 'Рассчитать'.
+Сама функция будет присылать ранее созданное Inline меню и текст 'Выберите опцию:'
+Создайте новую функцию get_formulas(call), которая:
+Будет обёрнута в декоратор callback_query_handler, который будет реагировать на текст
+ 'formulas'.
+Будет присылать сообщение с формулой Миффлина-Сан Жеора.
+Измените функцию set_age и декоратор для неё:
+Декоратор смените на callback_query_handler, который будет реагировать на текст
+ 'calories'.
+Теперь функция принимает не message, а call. Доступ к сообщению будет следующим
+ - call.message.
+По итогу получится следующий алгоритм:
+Вводится команда /start
+На эту команду присылается обычное меню: 'Рассчитать' и 'Информация'.
+В ответ на кнопку 'Рассчитать' присылается Inline меню: 'Рассчитать норму калорий'
+ и 'Формулы расчёта'
+По Inline кнопке 'Формулы расчёта' присылается сообщение с формулой.
+По Inline кнопке 'Рассчитать норму калорий' начинает работать машина состояний
+ по цепочке.
+'''
